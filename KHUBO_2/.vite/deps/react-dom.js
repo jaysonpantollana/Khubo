bf6c4e81ba4c722e52d0a6771115c68c69e9ec7a
Jayson E. Pantollana <129268177+jaysonpantollana@users.noreply.github.com>
2026-06-19 19:47:09 +0800

    Remove duplicate KHUBO_2/ directory, consolidate to root

--- KHUBO_2/.vite/deps/react-dom.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-PJTGFUNU.js";
-import "./chunk-ELDS2TUQ.js";
-import "./chunk-WOOG5QLI.js";
-export default require_react_dom();
--- KHUBO_2/src/hooks/useReducedMotion.ts
+++
@@ -1,3 +0,0 @@
-export function useReducedMotion() {
-  return false;
-}
